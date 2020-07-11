8dbec9e2fcd19542ebea973a263ea081e70da9fc
fadhlimulyana20 <baba.fadhli@gmail.com>
2020-07-11 14:50:12 +0700

    Adding static_root and static_url to django app

--- halo_bisnis/urls.py
+++ halo_bisnis/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('user_area.urls')),
@@ -29,3 +30,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
